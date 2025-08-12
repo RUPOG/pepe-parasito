9df46472914a9da150deb3672a96b6dad999e0cc
Esteban Sagman <sagmanesteban@gmail.com>
2025-08-12 14:11:46 -0300

    disparo gusanos y movimiento de boca

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 var direction : Vector2
 @onready var acceleration: int = 10
 @onready var max_speed = 250.0 # estaba en 200
@@ -15,6 +14,9 @@ var rota: bool = false
 var giro_d: bool = false 
 var giro_iz: bool = false
 var sangre: float = 0.5 
+
+var area_mause = false
+
 
 func _process(delta) -> void: 
 	
@@ -37,9 +39,39 @@ func _process(delta) -> void:
 		giro_d =false
 		rota = false
 		$rotacion.stop()
-		
+	
 # ---------------| FIN: CONTROL DE EVENTOS DE ROTACION |--------------#
-		
+
+
+
+# ---------------| INICIO: SEGUIMIENTO |--------------#
+
+	var pos_mouse = get_global_mouse_position()
+	#var base = abs(pos_mouse.x-$piboteB.global_position.x)
+	#var altura = abs(pos_mouse.y-$piboteB.global_position.y)
+	#var distancia = sqrt(base**2 + altura**2)
+	#print(distancia)
+	#var pos_mouse_global = get_global_mouse_position()  # posición global del mouse
+	#var pos_mouse_local = $piboteB.to_local(pos_mouse_global)  # posición local al nodo pivoteB
+	#print("Mouse local:", pos_mouse_local)
+	
+	if area_mause:
+		$piboteB.look_at(pos_mouse)
+
+	#print($limiteB.global_position.x)
+	
+	#var puntoBV = pos_mouse.y < $limiteB.position.y
+	#
+	#if puntoBV:
+		#$piboteB.to_local(pos_mouse)
+	
+# ---------------| FIN: SEGUIMIENTO |--------------#
+
+	if Input.is_action_just_pressed("lengua"):
+		$piboteB/boca.play("disparo")
+
+	if Input.is_action_just_released("lengua"):
+		$piboteB/boca.play("default")
 
 func _physics_process(delta: float) -> void:
 	
@@ -51,13 +83,13 @@ func _physics_process(delta: float) -> void:
 		if rotation_speed > -ROTACION_MAXIMA:
 			rotation_speed -= rotacion_aceleracion 
 		rotate(deg_to_rad(rotation_speed * delta)) 
-		print(rotation_speed)
+		#print(rotation_speed)
 		
 	if Input.is_action_pressed("RIGHT") and rota:
 		if rotation_speed < ROTACION_MAXIMA:
 			rotation_speed += rotacion_aceleracion 
 		rotate(deg_to_rad(rotation_speed * delta)) 
-		print(rotation_speed)
+		#print(rotation_speed)
 	
 	if rotation_speed != 0 and !rota: # desaceleracion del giro
 		if rotation_speed < 0:
@@ -68,14 +100,14 @@ func _physics_process(delta: float) -> void:
 		if rotation_speed == 2: # este fragmento solo evita el loop siempre y cuando la aceleracion de rotacion tega por final numero par
 			rotation_speed = 0
 		rotate(deg_to_rad(rotation_speed * delta))
-		print(rotation_speed)
+		#print(rotation_speed)
 
 	
 	if Input.is_action_pressed("SPACE"):
 		#Tiene que hacer un boost que haga que se mueva rápido en una dirección
 		$dash.start()
 		max_speed = 600
-		print(max_speed)
+		#print(max_speed)
 		velocity += input_vector.rotated(rotation) * 15
 	
 	if input_vector.y != 0:#Esto hace que vaya desacelerando de a poco en incremento de 3.
@@ -102,6 +134,8 @@ func _physics_process(delta: float) -> void:
 			$GPUParticles2D.restart() 
 		$GPUParticles2D.modulate = Color(1,1,1,sangre)
 	
+
+		
 	move_and_slide()
 
 
@@ -111,4 +145,13 @@ func _on_rotacion_timeout() -> void: # timer para que tarde un toque en arrancar
 
 func _on_dash_timeout() -> void: #timer para cambiar el maximo de velocidad al "pisar" charco de sangre (por ahora apretar barra)
 	var max_speed = 250.0
-	print(max_speed)
+	#print(max_speed)
+ 
+
+
+func _on_area_2d_mouse_entered() -> void:
+	area_mause = true
+
+
+func _on_area_2d_mouse_exited() -> void:
+	area_mause = false
